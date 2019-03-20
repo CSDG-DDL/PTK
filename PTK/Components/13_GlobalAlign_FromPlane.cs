2a7584eaa8a5e0daa3dd48270512aa3214885b01
Steinar Dyvik <Steinar.Dyvik@ntnu.no>
2019-03-20 21:03:26 +0100

    Yes

--- PTK/Components/13_GlobalAlign_FromPlane.cs
+++ PTK/Components/13_GlobalAlign_FromPlane.cs
@@ -82,8 +82,7 @@ namespace PTK.Components
             get
             {
                 //You can add image files to your project resources and access them like this:
-                // return Resources.IconForThisComponent;
-                return null;
+                return PTK.Properties.Resources.Alignment;
             }
         }
 
