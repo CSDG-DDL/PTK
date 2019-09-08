f75973543cc77e1a3874d9d80913821e73e873aa
johnhaddalmork <john@rallararkitekter.com>
2019-09-08 18:00:02 +0200

    FixingOrthogonal

--- PTK/Classes/DetailingGroupRules.cs
+++ PTK/Classes/DetailingGroupRules.cs
@@ -517,6 +517,7 @@ namespace PTK.Rules
                             else
                             {
                                 return false;
+
                                 
                             }
 
